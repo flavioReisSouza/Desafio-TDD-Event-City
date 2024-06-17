bf2e67b28b835d7ac6af9e3254ed68c60f998094
Flavio Reis <ext.flavios@unicid.edu.br>
2024-06-17 18:31:09 -0300

    Implementing the service and controller classes according to integration tests

--- src/main/java/com/devsuperior/demo/entities/Event.java
+++ src/main/java/com/devsuperior/demo/entities/Event.java
@@ -1,7 +1,5 @@
 package com.devsuperior.demo.entities;
 
-import java.time.LocalDate;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,70 +7,71 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "tb_event")
 public class Event {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	private LocalDate date;
-	private String url;
-	
-	@ManyToOne
-	@JoinColumn(name = "city_id")
-	private City city;
-	
-	public Event() {
-	}
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-	public Event(Long id, String name, LocalDate date, String url, City city) {
-		this.id = id;
-		this.name = name;
-		this.date = date;
-		this.url = url;
-		this.city = city;
-	}
+  private String name;
+  private LocalDate date;
+  private String url;
 
-	public Long getId() {
-		return id;
-	}
+  @ManyToOne
+  @JoinColumn(name = "city_id")
+  private City city;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+  public Event() {}
 
-	public String getName() {
-		return name;
-	}
+  public Event(Long id, String name, LocalDate date, String url, City city) {
+    this.id = id;
+    this.name = name;
+    this.date = date;
+    this.url = url;
+    this.city = city;
+  }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+  public Long getId() {
+    return id;
+  }
 
-	public LocalDate getDate() {
-		return date;
-	}
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-	public void setDate(LocalDate date) {
-		this.date = date;
-	}
+  public String getName() {
+    return name;
+  }
 
-	public String getUrl() {
-		return url;
-	}
+  public void setName(String name) {
+    this.name = name;
+  }
 
-	public void setUrl(String url) {
-		this.url = url;
-	}
+  public LocalDate getDate() {
+    return date;
+  }
 
-	public City getCity() {
-		return city;
-	}
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
 
-	public void setCity(City city) {
-		this.city = city;
-	}
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public City getCity() {
+    return city;
+  }
+
+  public void setCity(City city) {
+    this.city = city;
+  }
 }
--- src/main/java/com/devsuperior/demo/services/CityService.java
+++ src/main/java/com/devsuperior/demo/services/CityService.java
@@ -1,3 +1,46 @@
 package com.devsuperior.demo.services;
 
-public class CityService {}
+import com.devsuperior.demo.dto.CityDTO;
+import com.devsuperior.demo.entities.City;
+import com.devsuperior.demo.repositories.CityRepository;
+import com.devsuperior.demo.services.exceptions.DatabaseException;
+import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CityService {
+
+  @Autowired private CityRepository repository;
+
+  @Transactional(readOnly = true)
+  public List<CityDTO> findAll() {
+    List<City> list = repository.findAll(Sort.by("name"));
+    return list.stream().map(CityDTO::new).toList();
+  }
+
+  @Transactional
+  public CityDTO create(CityDTO dto) {
+    City entity = new City();
+    entity.setName(dto.getName());
+    entity = repository.save(entity);
+    return new CityDTO(entity);
+  }
+
+  @Transactional(propagation = Propagation.SUPPORTS)
+  public void delete(Long id) {
+    if (!repository.existsById(id)) {
+      throw new ResourceNotFoundException("Id not found " + id);
+    }
+    try {
+      repository.deleteById(id);
+    } catch (DataIntegrityViolationException e) {
+      throw new DatabaseException("Referenced integrity failure");
+    }
+  }
+}
